5d58f27a797ae388fe734a17119ac5c00bd3501d
AB7857 <AB7857@student.jamk.fi>
2023-04-21 20:43:27 +0300

    Some changes

--- FrameTempMonitor/app/src/main/java/com/nakkeez/frametempmonitor/preferences/SettingsActivity.kt
+++ FrameTempMonitor/app/src/main/java/com/nakkeez/frametempmonitor/preferences/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.nakkeez.frametempmonitor.preferences
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
